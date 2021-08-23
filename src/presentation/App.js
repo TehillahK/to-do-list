651357be5e467d2689985483ad7ef4a24825e249
Tehilah <tkangamba@hotmail.com>
2021-08-23 07:53:05 -0400

    started project

--- src/presentation/App.js
+++ src/presentation/App.js
@@ -1,26 +1,60 @@
 import './scss/main.scss'
 import ToDoList from "./components/ToDoList";
-import {getTasks} from "../logic/AccessTasks";
+import {getTasks,addTask} from "../logic/AccessTasks";
+import {useEffect, useState} from "react";
 
-function onClick(){
-    console.log("click")
+function onClick(text,updateList){
+    console.log(`you entered ${text}`)
+   // addTask(text)
+    updateList(text)
 }
 
 function Header(props){
+    const [text,setText]=useState("");
+    const update=props.update;
     return(
         <div className="todo-header">
-            <input placeholder={"Task"}/>
-            <a href="#" className="enter-button" onClick={onClick}>Add Task</a>
+            <input
+                onChange={event => setText(event.target.value)
+
+                }
+                value={text}
+                placeholder={"Task"}
+            />
+            <a href="#" className="enter-button" onClick={
+                ()=>{
+                    onClick(text,props.update)
+
+                }
+            }>Add Task</a>
         </div>
     )
 }
 function App() {
+    const [tasks ,setTasks]=useState(getTasks())
+
+
+    const updateList = (task) => {
+        const data=getTasks();
+
+       // setTasks([...tasks]);
+        const newTasks=[
+            ...tasks,
+            {
+                id:`task${task}`,
+                value:task
+            }
+        ]
+        setTasks(newTasks)
+        console.log("refresh")
+        console.log(tasks)
+    }
   return (
     <div className="container">
         <div className="flex">
             <div className="app">
-                <Header />
-                <ToDoList list={getTasks()} />
+                <Header update={updateList} />
+                <ToDoList list={tasks}  />
             </div>
         </div>
     </div>
